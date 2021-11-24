bc6a15790c6619d328e8829b18da79c3aee6cca5
ひより ことぶき <85456289+hiyorikotobuki@users.noreply.github.com>
2021-11-24 19:21:34 +0100

    ur oop bloat sir

--- emitter.cpp
+++ emitter.cpp
@@ -1,48 +1,58 @@
+#include <chrono>
+#include <iostream>
+#include <ratio>
+
 #include "screen.h"
 #include "math.h"
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+
+class Particle {
+public:
+    high_resolution_clock::time_point creation_time;
+    std::tuple<float, float> direction;
+    float velocity;
+
+    Particle() :
+        creation_time(high_resolution_clock::now()),
+        direction(sin(rand()), cos(rand())),
+        velocity(rand() % 100) {};
+
+    void draw(G &screen) {
+        float distance =
+            duration<float>(high_resolution_clock::now() - creation_time).count() * velocity;
+
+        if (distance < 0)
+            return;
+
+        screen.drawpixel(
+            static_cast<int>(distance * std::get<0>(direction) + 320),
+            static_cast<int>(distance * std::get<1>(direction) + 240)
+        );
+
+        if (distance > 400)
+            creation_time = high_resolution_clock::now();
+    }
+};
 
 int main(int argc, char* argv[])
 {
     G screen;
+    std::array<Particle, 5000> particles;
 
-    int total = 0;
-    int limit = 5000;
-    float directions[5000][4];
-    for(int i = 0; i < 5000; i++)
-            directions[i][2] = 1;
+    for(auto offset = 0ms; auto& p: particles)
+        p.creation_time += offset += 15ms;
+
     while(true) 
     {
-        if(total < limit && directions[total][2] == 1)
-        {   
-            // add a new direction
-            directions[total][0] = sin(rand());
-            directions[total][1] = cos(rand());
-            directions[total][2] = 1; // distance
-            directions[total][3] = 0.1*(float)(rand()%10);
-            total +=1;
-        }
-        // reset extended particles
-        for(int i = 0; i < total; i++)
-            if(directions[i][2] > 400)
-                directions[i][2]=1;
-        // draw all particles
-        for(int i = 0; i < total; i++)
-        {
-            float x = directions[i][0] * directions[i][2] + (640/2);
-            float y = directions[i][1] * directions[i][2] + (480/2);
-            screen.drawpixel((int)x,(int)y);
-        }
-        // increase all particle distance by 1
-        for(int i = 0; i < total; i++)
-        {
-            directions[i][2] += directions[i][3];
-        }
+        for(auto& p: particles)
+            p.draw(screen);
 
         screen.update(); 
         screen.input();
         screen.clearpixels();
         SDL_Delay(16);
-        
     }
     return 0;
 }
